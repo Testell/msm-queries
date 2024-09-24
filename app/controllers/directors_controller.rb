6aefb0e435d573122404e5b3bdedb0547d4bc2bc
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-24 21:33:27 +0000

    Finished director details page. Updated controller

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,14 @@ class DirectorsController < ApplicationController
   def get_directors
     render({ :template => "director_pages/list"})
   end
+
+  def get_director
+    the_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => the_id})
+
+    @the_director = matching_records.at(0)
+    
+    render({ :template => "director_pages/details"})
+  end
 end
