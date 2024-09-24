6c4eb83864322b91ca7de57a1e83c4d8600a0671
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-24 21:58:51 +0000

    almost done with youngest director. Just need to add the <p>

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,7 +9,15 @@ class DirectorsController < ApplicationController
     matching_records = Director.where({ :id => the_id})
 
     @the_director = matching_records.at(0)
-    
+    @matching_movies = Movie.where({ :director_id => @the_director.id})
     render({ :template => "director_pages/details"})
   end
+
+  def get_youngest_director
+    @youngest_director = Director.all.order({ :dob => :desc}).first().name
+    render({ :template => "director_pages/youngest_director"})
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "get_directors"})
-  get("/directors/:the_id", { :controller => "directors", :action => "get_director"})
   get("/directors/youngest", { :controller => "directors", :action => "get_youngest_director"})
   get("/directors/eldest", { :controller => "directors", :action => "get_eldest_director"})
+  get("/directors/:the_id", { :controller => "directors", :action => "get_director"})
 end
