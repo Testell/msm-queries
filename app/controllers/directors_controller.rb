3a8cb15adbbfb7280e85a2fbc95cba9920acd609
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-25 16:22:12 +0000

    finished eldest and youngest directors pages and logic

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class DirectorsController < ApplicationController
   def get_directors
     render({ :template => "director_pages/list"})
@@ -10,14 +12,23 @@ class DirectorsController < ApplicationController
 
     @the_director = matching_records.at(0)
     @matching_movies = Movie.where({ :director_id => @the_director.id})
+
     render({ :template => "director_pages/details"})
   end
 
   def get_youngest_director
-    @youngest_director = Director.all.order({ :dob => :desc}).first().name
+    @youngest_director = Director.order({ :dob => :desc}).first()
+    @youngest_director_age = @youngest_director.dob
+
     render({ :template => "director_pages/youngest_director"})
   end
 
+  def get_eldest_director
+    @eldest_director = Director.order({ :dob => :asc}).first()
+    @eldest_director_age = @eldest_director.dob
+
+    render({ :template => "director_pages/eldest_director"})
+  end
 
 
 end
